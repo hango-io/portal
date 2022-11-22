b47163497bd46147361eb2ddfc662a324f3f6ff0
yutao04 <yutao04@corp.netease.com>
2022-11-22 17:06:56 +0800

    feat: remove ServiceFilters content
     Signed-off-by: yutao04 <yutao04@corp.netease.com>

--- gateway-portal/src/main/java/org/hango/cloud/dashboard/envoy/service/impl/EnvoyServiceProxyServiceImpl.java
+++ gateway-portal/src/main/java/org/hango/cloud/dashboard/envoy/service/impl/EnvoyServiceProxyServiceImpl.java
@@ -1127,10 +1127,7 @@ public class EnvoyServiceProxyServiceImpl implements IServiceProxyService {
     @Override
     public Map<String, String> createServiceFilters(RegistryCenterDto registry) {
         Map<String, String> filters = new HashMap<>();
-        if (registry != null && registry.getRegistryAlias().contains(Const.NSF_EUREKA_ALIAS)) {
-            PermissionScopeDto projectScopeDto = gatewayProjectService.getProjectScopeDto(ProjectTraceHolder.getProId());
-            filters.put(Const.PREFIX_LABEL + Const.PROJECT_CODE, projectScopeDto.getPermissionScopeEnName());
-        }
+        // 此处扩展服务过滤条件
         return filters;
     }
 }
